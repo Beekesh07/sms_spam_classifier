f06276a16be58a6d8423d350f34e17dec85dc20e
Beekesh Singh <beekeshsingh0007@gmail.com>
2025-01-03 14:35:34 +0530

    chnages done

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps=PorterStemmer()
+
+nltk.download('punkt_tab')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
 st.title('SMS Spam Classifier')
 
 tf=pickle.load(open('vectorizer.pkl','rb'))
